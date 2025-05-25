fb8e0971b5a408c24c932fbcfc9a1dc187697ab5
kandeepvar <48804815+kandeepvar@users.noreply.github.com>
2025-05-25 19:45:50 -0400

    Add files via upload

---
+++ Day1/primitiveDataType.js
@@ -0,0 +1,15 @@
+//firstName declaration as using Let (Not with Var) and view in console 
+let firstName = "Kannan";
+console.log("firstName:", firstName,  typeof firstName);
+
+//companyname,mobile#,isAutomation,hasPlaywright declaration as using Let (Not with Var) and view in console 
+let companyName = "FiServe";
+let mobileNumber = 7819717017;
+let isAutomation = false;
+let hasPlaywright;
+
+
+console.log("companyName:", companyName,  typeof companyName);
+console.log("mobileNumber:", mobileNumber,  typeof mobileNumber);
+console.log("isAutomation:", isAutomation,  typeof isAutomation);
+console.log("hasPlaywright:", hasPlaywright,  typeof hasPlaywright);
